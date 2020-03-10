da9819f4a89ebca4ad8af340202de71550327576
rooke1234 <nooknicksoawalak_@hotmail.com>
2020-03-10 16:07:55 +0700

    addfinger,dayoff,card,takeoff

---
+++ src/pages/di-emdayoff/di-emdayoff.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { DiEmdayoffPage } from './di-emdayoff';
+
+@NgModule({
+  declarations: [
+    DiEmdayoffPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(DiEmdayoffPage),
+  ],
+})
+export class DiEmdayoffPageModule {}
---
+++ src/pages/di-emdayoff/di-emdayoff.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the DiEmdayoffPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-di-emdayoff',
+  templateUrl: 'di-emdayoff.html',
+})
+export class DiEmdayoffPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad DiEmdayoffPage');
+  }
+
+}
